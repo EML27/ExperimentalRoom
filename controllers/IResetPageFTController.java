2691047bc6ce17172d0bc13419d3146081d86bed
Vladislav <vladislav4869@gmail.com>
2019-04-19 17:23:08 +0300

    Ho-ho 向かって来るのか？

---
+++ controllers/IResetPageFTController.java
@@ -0,0 +1,11 @@
+package controllers;
+
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+public interface IResetPageFTController extends Initializable {
+
+    @FXML
+    void closeWindow(Event event);
+}
